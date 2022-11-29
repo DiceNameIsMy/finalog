213ce4ff2e8d3b12e667386120d3a9ffb56b5231
DiceNameIsMy <dicenameismy@gmail.com>
2022-11-29 21:55:33 +0100

    Add .from_repo methods to Category and User schemes

--- src/domain/schemes/categories.py
+++ src/domain/schemes/categories.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 import uuid
 
+import repository as repo
+
 
 @dataclass
 class Category:
@@ -8,3 +10,7 @@ class Category:
     name: str
 
     user_id: uuid.UUID
+
+    @classmethod
+    def from_repo(cls, category: repo.schemes.Category) -> "Category":
+        return cls(id=category.id, name=category.name, user_id=category.user_id)
--- src/domain/schemes/users.py
+++ src/domain/schemes/users.py
@@ -2,7 +2,15 @@ from dataclasses import dataclass
 import uuid
 
 
+import repository
+
+
 @dataclass
 class User:
     id: uuid.UUID
     email: str
+    hashed_password: str
+
+    @classmethod
+    def from_repo(cls, user: repository.schemes.User) -> "User":
+        return cls(id=user.id, email=user.email, hashed_password=user.hashed_password)
